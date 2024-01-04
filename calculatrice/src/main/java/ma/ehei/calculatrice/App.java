b63b91c612edb5cdad46083c9e660d6a8991676f
Bouayachi Abderrahim <abderrahimbale3@gmail.com>
2024-01-04 19:26:37 +0100

    Résolution des conflits dans Calculatrice.java and app.java

--- calculatrice/src/main/java/ma/ehei/calculatrice/App.java
+++ calculatrice/src/main/java/ma/ehei/calculatrice/App.java
@@ -2,11 +2,11 @@ package ma.ehei.calculatrice;
 
 public class App {
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		System.out.println(Calculatrice.add(5, 5));
-
 		System.out.println(Calculatrice.multiplication(6, 5));
-
-		
-
+=======
+		System.out.println(Calculatrice.add(10, 5));
+>>>>>>> e8167f7 (Correction la fonction add By Student 2)
 	}
 }
--- calculatrice/src/main/java/ma/ehei/calculatrice/Calculatrice.java
+++ calculatrice/src/main/java/ma/ehei/calculatrice/Calculatrice.java
@@ -2,12 +2,19 @@ package ma.ehei.calculatrice;
 
 public class Calculatrice {
 	public static int add(int a, int b) {
-		return a+b; 
+<<<<<<< HEAD
+		return a+b;
 	}
+
 	public static int multiplication(int a, int b) {
-		return a*b; 
+		return a * b;
 	}
-	public static int multiplication(int a, int b) {
-		return a*b; 
-	}
+=======
+		return a + b; // à corriger par a+b
 }
+
+	public static int multiplication(int a, int b) {
+		return a * b;
+	}
+>>>>>>> e8167f7 (Correction la fonction add By Student 2)
+		}
